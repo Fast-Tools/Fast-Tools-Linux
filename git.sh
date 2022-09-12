793586ab8128928bb56fc13f9e7b07e1ee66c390
sebi-vscode2 <stoklosasebastian114@gmail.com>
2022-09-12 22:14:15 +0000

    Mejoras Añadidas
    Se añadio pullsh y descargar git si no estaba descargado en arch linux y en debian/ubuntu o distribuciones basadas en esas.

--- git.sh
+++ git.sh
@@ -3,7 +3,10 @@ echo "Que queres Configurar?"
 echo "1. Pull y Push en un solo comando"
 read opcion
 if [ opcion = 1 ];
-then 
-echo "Ok, usar ft pullsh para que puedas hacer eso"
+then
+sudo apt update
+sudo apt install git -y
+sudo pacman -S git 
+echo "Ok, usar ft-pullsh para que puedas hacer eso"
 fi
 exit 1
--- install.sh
+++ install.sh
@@ -2,12 +2,15 @@
 cd /home
 sudo mkdir fasttools
 cd fasttools
-sudo wget -q https://fasttools.ivorcorp.com.ar/ft-linux/fast-tools.sh 
-sudo wget -q https://fasttools.ivorcorp.com.ar/ft-linux/ft.sh
-sudo wget -q https://fasttools.ivorcorp.com.ar/ft-linux/git.sh
+sudo wget -q https://fasttools.ivorcorp.com.ar/Fast-Tools-Linux/fast-tools.sh 
+sudo wget -q https://fasttools.ivorcorp.com.ar/Fast-Tools-Linux/ft.sh
+sudo wget -q https://fasttools.ivorcorp.com.ar/Fast-Tools-Linux/git.sh
+sudo wget -q https://fasttols.ivorcorp.com.ar/Fast-Tools-Linux/ft-pullsh.sh
+sudo chmod +777 ./ft-pullsh.sh
 sudo chmod +777 ./git.sh
 sudo chmod +777 ./ft.sh
 sudo chmod +777 ./fast-tools.sh
+sudo ln -s /home/fasttols/ft-pullsh.sh /usr/local/bin/pullsh
 sudo ln -s /home/fasttools/git.sh /usr/local/bin/git
 sudo ln -s /home/fasttools/ft.sh /usr/local/bin/ft
 sudo ln -s /home/fasttools/fast-tools.sh /usr/local/bin/fasttools
